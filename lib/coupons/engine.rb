30b366303481d04b241b3fc8db4a12a89987fde4
Kirill Shirinkin <fodojyko@gmail.com>
2016-06-20 12:28:20 +0200

    Do not append migrations to main app

--- lib/coupons/engine.rb
+++ lib/coupons/engine.rb
@@ -2,14 +2,6 @@ module Coupons
   class Engine < Rails::Engine
     isolate_namespace Coupons
 
-    initializer 'coupons.append_migrations' do |app|
-      next if app.root.to_s == root.to_s
-
-      config.paths['db/migrate'].expanded.each do |path|
-        app.config.paths['db/migrate'].push(path)
-      end
-    end
-
     initializer 'coupons.assets' do |app|
       app.config.assets.precompile += %w[coupons.css coupons.js]
     end
